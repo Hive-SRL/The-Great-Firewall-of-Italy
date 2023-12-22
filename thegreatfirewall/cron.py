a76013b6d0183ed8c63d0712addbcfcb3de86932
Federico De Martin <fededema96@gmail.com>
2023-12-22 10:24:37 +0100

    sistemato requirements

--- thegreatfirewall/cron.py
+++ thegreatfirewall/cron.py
@@ -15,6 +15,5 @@ class RefreshCredentials(CronJobBase):
     RUN_EVERY_MINS = 5
     schedule = Schedule(run_every_mins=RUN_EVERY_MINS)
     code = 'thegreatfirewall.check_credentials'
-
     def do(self):
         pass
