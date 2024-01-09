107bf99cf7e0b32f692d268cf369899cded4cac1
Federico De Martin <fededema96@gmail.com>
2024-01-09 12:07:29 +0100

    Add table PiracyShieldAuth

--- thegreatfirewall/admin.py
+++ thegreatfirewall/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import TicketPS, FqdnPS, IPv4PS, IPv6PS
+from .models import TicketPS, FqdnPS, IPv4PS, IPv6PS, PiracyShieldAuth
 
 admin.site.register(TicketPS)
 admin.site.register(FqdnPS)
 admin.site.register(IPv4PS)
 admin.site.register(IPv6PS)
+admin.site.register(PiracyShieldAuth)
 
